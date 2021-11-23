333ba396d2bdd1e92f7199f8c27d4f0fa7d0ed6d
elddc <emmachen2010@gmail.com>
2021-11-22 18:26:32 -0600

    Added basic timer in seconds and minutes
    Assumptions about end case
    * Will not extend into hours
    * Will not need to pause/resume/reset
    * Dark theme is preferable (purely aesthetic reasons)

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TouchableHighlight, Dimensions, Platform} from 'react-native';
+import Constants from 'expo-constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Timer from './components/timer';
+
+//ensures reasonably consistent sizing
+//assumes (mobile) app will always open in full screen (not split with another app)
+let metric = Platform.OS === 'web' ? 'window' : 'screen';   //best fit for different platforms
+let vw = Dimensions.get(metric).width / 100;
+let vh = Dimensions.get(metric).height / 100;
+if (vw > vh){   //swaps dimensions if opened in landscape
+	[vw, vh] = [vh, vw];
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const App = () => {
+	const [timer, setTimer] = useState(false); //whether timer is active
+	const [reset, setReset] = useState(0) //dev purposes only
+
+	//must toggle in order of start, stop, reset (see timer component for more info)
+	const toggleTimer = () => setTimer(!timer);
+
+	return (
+		<View style={s.container}>
+			<Timer active={timer} reset={reset} />
+			<TouchableHighlight style={[s.button, {backgroundColor: (timer ? '#de1245' : '#16c6a3')}]} onPress={toggleTimer}>
+				<Text style={s.buttonText}>{timer ? 'Stop' : 'Start'}</Text>
+			</TouchableHighlight>
+			<TouchableHighlight style={[s.button]} onPress={() => setReset(reset + 1)}>
+				<Text style={s.buttonText}>Reset</Text>
+			</TouchableHighlight>
+		</View>
+	);
+}
+
+const s = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		paddingTop: Constants.statusBarHeight,
+		backgroundColor: 'black',
+		padding: 8,
+	},
+	timerContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	button: {
+		margin: 1*vh,
+		padding: 3*vw,
+		minHeight: 12*vw,
+		minWidth: 25*vw,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 5*vh,
+	},
+	buttonText: {
+		color: 'white',
+		fontSize: 18,
+		fontWeight: 'bold',
+	},
+	timerText: {
+		color: 'white',
+		fontSize: 52,
+	},
 });
+
+export default App;
---
+++ components/timer.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+//maximum unit is in minutes (assumes will never need hours)
+const Timer = ({active, reset}) => {
+	const [elaspedTime, setElapsedTime] = useState(0); //time in seconds
+	const [timer, setTimer] = useState();
+
+	//note: this will not handle pausing (stopping after a reset) because it is not needed in the final use case
+	//display an "are you sure" alert before ending in final version, in case of accidental misclicks
+	useEffect(() => {
+	    if (active && elaspedTime === 0) {
+		    let time = Date.now();
+		    setTimer(setInterval(() => {
+			    setElapsedTime(Math.floor((new Date() - time)/1000));
+		    }, 250));
+	    }
+		else if (!active) {
+			clearInterval(timer);
+			setTimer(false);
+	    }
+
+		return () => clearInterval(timer);
+	}, [active]);
+
+	//reset mechanic used only for development purposes
+	useEffect(() => {
+	    if (reset > 0)
+		    setElapsedTime(0);
+	}, [reset]);
+
+	return (
+		<View style={s.timerContainer}>
+			<Text style={s.timerText}>
+				{Math.floor(elaspedTime / 60).toString().padStart(2, '0')}
+				:
+				{(elaspedTime % 60).toString().padStart(2, '0')}
+			</Text>
+		</View>
+	);
+}
+
+const s = StyleSheet.create({
+	timerContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: 15,
+	},
+	timerText: {
+		color: 'white',
+		fontSize: 52,
+	},
+});
+
+export default Timer;
